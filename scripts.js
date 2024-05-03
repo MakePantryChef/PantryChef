b4be4b28b80421ad8585224d6e16c4503ad6dd37
Quang Vo <qhvo@berkeley.edu>
2024-05-03 13:09:18 -0700

    added image generation

--- scripts.js
+++ scripts.js
@@ -42,27 +42,47 @@ ingredientList.addEventListener('click', function(event) {
     
 });
 
+const outputBox = document.getElementById("output-box");
+const loadingScreen = document.getElementById('loading-screen');
+outputBox.style.visibility = "hidden";
+
 generateButton.addEventListener('click', async() => {
-    const ingredientString = Array.from(ingredientList.children)
-        .map(item => item.textContent.split('X')[0].trim())
-        .join(', ');
+    loadingScreen.style.visibility = 'visible';
+    if (ingredients.length > 0) {
+        const ingredientString = Array.from(ingredientList.children)
+            .map(item => item.textContent.split('X')[0].trim())
+            .join(', ');
 
-    const outputBox = document.getElementById("output-box")
+        const response = await fetch(
+            'https://noggin.rea.gent/prickly-otter-9071',
+            {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer rg_v1_q4avjj0so5rv36g92lhca3yjexnp6oe222rn_ngk',
+            },
+            body: JSON.stringify({
+                // fill variables here.
+                "ingredients": ingredientString,
+            }),
+            }
+        ).then(response => response.text());
 
-    const response = await fetch(
-        'https://noggin.rea.gent/prickly-otter-9071',
-        {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer rg_v1_q4avjj0so5rv36g92lhca3yjexnp6oe222rn_ngk',
-        },
-        body: JSON.stringify({
-            // fill variables here.
-            "ingredients": ingredientString,
-        }),
-        }
-    ).then(response => response.text());
+        outputBox.value = response;
+        const textArray = response.split("Ingredients");
+        const dishName = textArray[0];
+        var img = document.createElement('img');
+        img.src = `https://noggin.rea.gent/upper-leopard-9184?key=rg_v1_utr7aba076ighlle108pv6ywmygpvscv001i_ngk&food=${dishName}`;
+        img.width = 500;
+        img.height = 500;
+        var container = document.getElementById("imageContainer");
+        container.appendChild(img);
 
-    outputBox.value = response;
+        outputBox.style.visibility = "visible";
+        loadingScreen.style.visibility = 'hidden';
+    } else {
+        loadingScreen.style.visibility = 'hidden';
+        alert("Error: Ingredients list is empty")
+    }
+    
 })
